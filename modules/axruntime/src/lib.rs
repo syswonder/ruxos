dcfd879bbc30c2905def4c373d67bb5a44e3e6f4
AuYang261 <459461160@qq.com>
2023-09-14 15:30:57 +0800

    fix: add a if statement in update_timer

--- modules/axruntime/src/lib.rs
+++ modules/axruntime/src/lib.rs
@@ -276,11 +276,14 @@ fn init_interrupt() {
     static NEXT_DEADLINE: u64 = 0;
 
     fn update_timer() {
+        let now_ns = axhal::time::current_time_nanos();
         // Safety: we have disabled preemption in IRQ handler.
         let mut deadline = unsafe { NEXT_DEADLINE.read_current_raw() };
-        deadline += PERIODIC_INTERVAL_NANOS;
-        unsafe { NEXT_DEADLINE.write_current_raw(deadline) };
-        axhal::time::set_oneshot_timer(deadline);
+        if now_ns >= deadline {
+            deadline = now_ns + PERIODIC_INTERVAL_NANOS;
+            unsafe { NEXT_DEADLINE.write_current_raw(deadline) };
+            axhal::time::set_oneshot_timer(deadline);
+        }
     }
 
     axhal::irq::register_handler(TIMER_IRQ_NUM, || {
