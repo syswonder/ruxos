138c52115cfcc21f2dfab1d917af36e355751897
oslab <1466397747@qq.com>
2023-09-15 10:29:06 +0800

    add rtc

--- apps/c/systime/main.c
+++ apps/c/systime/main.c
@@ -12,8 +12,8 @@ int main()
     printf("Seconds: %ld\n", tv.tv_sec);
     printf("Microseconds: %ld\n", tv.tv_usec);
 
-    //usleep(3000000);
-    sleep(3);
+    usleep(3000000);
+    //sleep(3);
 
     if (gettimeofday(&tv, NULL) != 0 ) {
         perror("gettimeofday");
@@ -27,7 +27,6 @@ int main()
     new_time.tv_sec = 1731110400; 
     new_time.tv_usec = 0;
 
-    // 使用 settimeofday 设置新的系统时间
     if (settimeofday(&new_time, NULL) != 0 ) {
         perror("settimeofday");
         return 1;
