58a599303f3e932c822eee2381c86f14d07b29c9
Ruohai Zhu <1132370844@qq.com>
2024-03-18 00:29:31 +0800

    Add support for Raspi4

--- scripts/make/raspi4.mk
+++ scripts/make/raspi4.mk
@@ -89,6 +89,6 @@ openocd:
 ## Start GDB session
 ##------------------------------------------------------------------------------
 gdb: RUSTC_MISC_ARGS += -C debuginfo=2
-gdb: $(KERNEL_ELF)
+gdb: $(OUT_ELF)
 	$(call color_header, "Launching GDB")
-	@$(DOCKER_GDB) gdb-multiarch -q $(KERNEL_ELF)
+	@$(DOCKER_GDB) gdb-multiarch -q $(OUT_ELF)
