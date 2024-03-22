9c7e89309c208772d47fcd3c585bec2ee3e6966d
Sssssaltyfish <47731209+Sssssaltyfish@users.noreply.github.com>
2024-03-23 00:50:43 +0800

    Patched ecall ids to support RuxOS under riscv

--- run-sv.sh
+++ run-sv.sh
@@ -2,4 +2,5 @@
 
 git submodule update --init --recursive --remote
 export CROSS_COMPILE=riscv64-linux-musl-
-make -C patches/opensbi PLATFORM=generic; make run APP=apps/c/helloworld ARCH=riscv64 FEATURES=musl,multitask MUSL=y LOG=info
+make -C patches/opensbi PLATFORM=generic
+make run APP=apps/c/helloworld ARCH=riscv64 FEATURES=musl,multitask MUSL=y LOG=info
