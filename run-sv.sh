0dc2cf078f730dc3ece037805cf29e72a018d077
Sssssaltyfish <47731209+Sssssaltyfish@users.noreply.github.com>
2024-03-24 20:16:19 +0800

    Changed run-sv to run libc-bench

--- run-sv.sh
+++ run-sv.sh
@@ -3,4 +3,4 @@
 git submodule update --init --recursive --remote
 export CROSS_COMPILE=riscv64-linux-musl-
 make -C patches/opensbi PLATFORM=generic
-make run APP=apps/c/helloworld ARCH=riscv64 FEATURES=musl,multitask MUSL=y LOG=info
+make run APP=apps/c/libc-bench ARCH=riscv64 FEATURES=musl,multitask MUSL=y LOG=warn
