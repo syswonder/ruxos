01b0049ef55611e50c11618582534ee88d36a904
ZZJJWarth <2678328250@qq.com>
2025-05-29 15:19:47 +0800

    fix: riscv compilation error caused by incompatible asm

--- modules/ruxhal/src/arch/riscv/context.rs
+++ modules/ruxhal/src/arch/riscv/context.rs
@@ -135,36 +135,35 @@ unsafe extern "C" fn context_switch(_current_task: &mut TaskContext, _next_task:
     naked_asm!(
         "
         // save old context (callee-saved registers)
-        STR     ra, a0, 0
-        STR     sp, a0, 1
-        STR     s0, a0, 2
-        STR     s1, a0, 3
-        STR     s2, a0, 4
-        STR     s3, a0, 5
-        STR     s4, a0, 6
-        STR     s5, a0, 7
-        STR     s6, a0, 8
-        STR     s7, a0, 9
-        STR     s8, a0, 10
-        STR     s9, a0, 11
-        STR     s10, a0, 12
-        STR     s11, a0, 13
+        sd     sp, 8(a0)
+        sd     s0, 16(a0)
+        sd     s1, 24(a0)
+        sd     s2, 32(a0)
+        sd     s3, 40(a0)
+        sd     s4, 48(a0)
+        sd     s5, 56(a0)
+        sd     s6, 64(a0)
+        sd     s7, 72(a0)
+        sd     s8, 80(a0)
+        sd     s9, 88(a0)
+        sd     s10, 96(a0)
+        sd     s11, 104(a0)
 
         // restore new context
-        LDR     s11, a1, 13
-        LDR     s10, a1, 12
-        LDR     s9, a1, 11
-        LDR     s8, a1, 10
-        LDR     s7, a1, 9
-        LDR     s6, a1, 8
-        LDR     s5, a1, 7
-        LDR     s4, a1, 6
-        LDR     s3, a1, 5
-        LDR     s2, a1, 4
-        LDR     s1, a1, 3
-        LDR     s0, a1, 2
-        LDR     sp, a1, 1
-        LDR     ra, a1, 0
+        ld     s11, 104(a1)
+        ld     s10, 96(a1)
+        ld     s9, 88(a1)
+        ld     s8, 80(a1)
+        ld     s7, 72(a1)
+        ld     s6, 64(a1)
+        ld     s5, 56(a1)
+        ld     s4, 48(a1)
+        ld     s3, 40(a1)
+        ld     s2, 32(a1)
+        ld     s1, 24(a1)
+        ld     s0, 16(a1)
+        ld     sp, 8(a1)
+        ld     ra, 0(a1)
 
         ret",
     )
